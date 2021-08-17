6466ca6349feca553510544a5826595fc5197a7c
Mxchaeltrxn <tranmichael97@gmail.com>
2021-08-17 19:11:04 +1000

    Get storybook working with webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../components/**/*.stories.js"],
   addons: ["@storybook/addon-essentials"],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      include: path.resolve(__dirname, "../"),
+      use: [
+        {
+          loader: require("styled-jsx/webpack").loader,
+        },
+      ],
+    });
+    return config;
+  },
 };
